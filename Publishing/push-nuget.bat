368655d984031979e77bbad0b787436196fbeb81
Paul Parau <paul.parau@recognos.ro>
2018-05-07 19:32:46 +0300

    Bump version, update changelog, push nuget

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,3 +1,3 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.5.5.nupkg -Source https://api.nuget.org/v3/index.json
+..\.nuget\NuGet.exe push Metrics.NET.0.5.6-pre.nupkg -Source https://api.nuget.org/v3/index.json
 ..\.nuget\NuGet.exe push Nancy.Metrics.0.4.7.nupkg -Source https://api.nuget.org/v3/index.json
 ..\.nuget\NuGet.exe push Owin.Metrics.0.5.5.nupkg -Source https://api.nuget.org/v3/index.json
